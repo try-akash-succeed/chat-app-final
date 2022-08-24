1f175b012401207a7e529f801aa60c8ed989b11e
AKASH YADAV <prince.lasskicker@gmail.com>
2022-08-24 17:52:45 +0530

    entire chat app completed

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Navbar from "./components/Navbar";
 
 import { auth } from "./firebase";
 import {useAuthState} from 'react-firebase-hooks/auth'
+import Chat from "./components/Chat";
 
 
 const style ={
   appContainer: `max-w-[728px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
-}
+};
 
 function App() {
 
   const [user] = useAuthState(auth)
-  //console.log(user)
+  console.log(user)
 
   return (
     <div className={style.appContainer}>
@@ -21,7 +22,7 @@ function App() {
           {/*Navbar*/}
           <Navbar/>
           {/*Chat component*/}
-
+          {user ? <Chat/> : null}
          </section>
     </div>
   );
